5dab3e83052a9f8d77703e528da4a143eed5c049
Michaa04 <michael.zarza@alumnos.ispi4038.edu.ar>
2026-08-10 19:31:36 -0300

    Agrego lo realizado en clase
    Fecha: 10/08

---
+++ Practica_clases/JS/clase_6/inner.js
@@ -0,0 +1,53 @@
+// x= windows innerwith
+// y= windows innerheigth
+
+//location.href assing("url")
+//windows.history.go(-1, 1)
+
+//cookies: recauda información 
+//cookies: localstorage, seccionstorage y indexedDB, cookies
+//Ejemplos:
+
+//Guardar informacion
+//localStorage.setItem("Pepa", "holamundo")
+//
+//localStorage.getItem("Pepa")
+localStorage.removeItem("pepe")
+localStorage.clear("pepa")
+
+sessionStorage.setItem("micha", "hola micha");
+
+// Funciones localstorage
+function probarStorage_guardar() {
+    localStorage.setItem("micha", "hola micha");
+    alert("Guardado micha");
+}
+
+function probarStorage_remove() {
+    localStorage.removeItem("micha");
+    alert("Eliminado pepa");
+}
+
+function probarStorage_clear() {
+    localStorage.clear();
+    alert("Eliminado todos");
+}
+//Ejemplo de uso
+function guardarEmail() {
+    localStorage.setItem()
+}
+
+
+//Asincronismo 
+//Callbacks: Se le pasa una funcion
+function greet(name) {
+    alert("nombre: " + name);
+}
+
+function processAccess(Callbacks) {
+    var name = prompt("Ingrese_nombre");
+    Callbacks(name);
+}
+
+processAccess(greet);
+  
